c92d395215cba1651c8bffe68a2ddd3ef34ec7b0
stemineo <ste.mineo@gmail.com>
2015-03-17 15:35:57 +0100

    I have edited the file cachematrix.R by adding my solutions fot the functions makeCacheMatrix and cacheSolve, along with short comments describing these functions

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,36 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+     
+     invmtx <- NULL # store the result of inversion here into invmtx
+     set <- function(y) {
+          x <<- y
+          invmtx <<- NULL # initialize invmtx and assign it a value NULL
+          # in an environment that differs from the current
+     }
+     get <- function() x # return the input matrix
+     setinvrs <- function(inverse) invmtx <<- inverse # set the inversed matrix
+     getinvrs <- function() invmtx # return the inversed matrix
+     
+     # return a list that contains the functions defined above
+     list(set=set, get=get, 
+          setinvrs=setinvrs, 
+          getinvrs=getinvrs)
 }
+
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+     invmtx <- x$getinvrs() # get the inversed matrix from object x
+     if(!is.null(invmtx)) {  # check if invmtx has already been calculated 
+          # (invmtx is not NULL, as initialized, if calculated)  
+          message("getting cached data")
+          return(invmtx) # if invmtx has already been calculated, return it, otherwise...
+     }
+     mtx <- x$get() # get the input matrix 
+     invmtx <- solve(mtx) # calculate the inverse of the input matrix
+     x$setinvrs(invmtx) # set the inverse matrix as object
+     invmtx # returns the inverse matrix
 }
